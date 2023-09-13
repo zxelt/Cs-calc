53afd51d6eeddd7839fee04e221dcc0c7302c31c
Axel <108530057+zxelt@users.noreply.github.com>
2023-09-13 19:03:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+
+int First;
+int Second;
+int Result = 0;
+string Opperator;
+Console.Write("Enter First: ");
+First = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Second: ");
+Second = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Opperator: ");
+Opperator = Console.ReadLine();
+switch (Opperator)
+{
+    case "+":
+        Result = First + Second;
+        break;
+    case "-":
+        Result = First - Second;
+        break;
+    case "*":
+        Result = First * Second;
+        break;
+    case "/":
+        Result = First / Second;
+        break;
+    default: Console.Write("Invalid Operator");
+        Result = 0;
+        break;
+}
+
+Console.WriteLine(Result.ToString());
